0f4e00637733668b381a543297d77465ef9141d1
Martin Reynolds <martinandtoni@btinternet.com>
2021-02-25 22:05:55 +0000

    Added procedural function passing amounts into the function.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,18 @@
 // Tip Calculator JS //
 
 //Get initial amounts from prompt boxes, using + to ocnvert string to number.
-var preTip = +prompt('How much does your bill come to?');
-var tipPercent = +prompt('What rate are you tipping at? excluding the % sign.')
+var preTipFromPrompt = +prompt('How much does your bill come to?');
+var tipPercentFromPrompt = +prompt('What rate are you tipping at? excluding the % sign.')
 
 //Calculate tip amount then add to preTtip to give total
-var tipAmount = (preTip / 100) * tipPercent;
-var newBillAmount = preTip + tipAmount;
 
-alert('Your total bill is £'+newBillAmount.toFixed(2)+', made up of your initial bill of £'+preTip.toFixed(2)+' and your tip of £'+tipAmount.toFixed(2))+'.';
+function tipCalculator(preTip, tipPercent){
+    var tipAmount = (preTip / 100) * tipPercent;
+    var newBillAmount = preTip + tipAmount;
+
+    return (
+        alert('Your total bill is £'+newBillAmount.toFixed(2)+', made up of your initial bill of £'+preTip.toFixed(2)+' and your tip of £'+tipAmount.toFixed(2))+'.'
+    )
+};
+
+tipCalculator(preTipFromPrompt, tipPercentFromPrompt);
